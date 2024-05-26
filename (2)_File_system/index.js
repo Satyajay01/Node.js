d47efe9ae7738b6d3e7ff301ec098ad91070235b
satyajay01 <satyajaydibya0@gmail.com>
2024-05-26 14:35:12 +0530

    File system

--- (2)_File_system/index.js
+++ (2)_File_system/index.js
@@ -1,18 +1,28 @@
-const fs = require('fs');
+// const fs = require('fs');
+import fs from 'fs'
 
 
 // 1 method synchronous---
+// File Read
 const text1 =fs.readFileSync('./text.txt','utf-8');
 console.log(text1)
-
 
 
 // 2 method asynchronous---
 fs.readFile("text.txt",'utf-8',(err, text2)=>{
     console.log(text2)
 
-
     //only time checking purpose
     const t1 = performance.now();
     console.log("time checking",t1)
 })
+
+// ----------------------
+
+
+// file write
+const content = 'this is a writeFile text'
+fs.writeFile('text_write_File.txt',content,()=>{
+    console.log("file crated...")
+});
+
