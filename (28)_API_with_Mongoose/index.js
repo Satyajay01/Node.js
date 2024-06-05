456318e79772a353c36f31714f1347ebde60a838
satyajay01 <satyajaydibya0@gmail.com>
2024-06-05 23:59:05 +0530

    Mongoose API Methods

---
+++ (28)_API_with_Mongoose/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import './config/config.js';
+import Product from './config/product.js'
+
+const app = express();
+app.use(express.json());
+
+// API Methods-------------
+
+// Get method
+app.get("/read", async(req, res)=>{
+    const data = await Product.find();
+    res.send(data)
+    console.log(data)
+})
+
+
+// Post method
+app.post("/create", async (req, res)=>{
+    const data = new Product(req.body);
+    const result = await data.save();
+    console.log(result);
+    res.send(result)
+});
+
+
+
+
+// Delete method
+// url: (http://localhost:5000/delete/6660aa46d779cc964b89d4cf)
+
+app.delete("/delete/:_id", async (req, res)=>{
+    const data = await Product.deleteOne(req.params);
+    res.send(data)
+    console.log("Data Deleted: ",req.params);
+})
+
+app.listen(5000,()=>{
+    console.log("Server is Started...!");
+})
